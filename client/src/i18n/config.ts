e7cd844453dbdc90f875a061cd9fd61c07ab0604
Iwaway <belokonegor2000@gmail.com>
2024-11-27 16:15:59 +0200

    SCRUM-48 add: i18n

---
+++ client/src/i18n/config.ts
@@ -0,0 +1,26 @@
+import { LOCALS } from "../constants";
+import en from "./en/index.json";
+import ua from "./ua/index.json";
+import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+export const resources = {
+  [LOCALS.EN]: {
+    translation: en,
+  },
+  [LOCALS.UA]: {
+    translation: ua,
+  },
+};
+
+i18next.use(initReactI18next).use(LanguageDetector).init({
+  debug: true,
+  resources,
+  fallbackLng: LOCALS.EN,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18next;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./i18n/config";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 
